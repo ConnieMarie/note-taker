2fb37cb2d14d1def4e5b299f6e76ddc1684cde31
ConnieMarie <constance.marie.strahan@gmail.com>
2022-09-27 10:37:22 -0500

    comments

--- server.js
+++ server.js
@@ -7,20 +7,25 @@ const app = express();
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+
 // parse incoming JSON data
 app.use(express.json());
+
+// set up middleware to instruct the server to make static files available
 app.use(express.static('public'));
 
+// set route to server root and respond with the file I want he server to read
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
+  // set route to transfer data to notes endpoint
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
   });
 
-  app.get('/api/notes', (req, res) => {
-    
+  // set up server to send json
+  app.get('/api/notes', (req, res) => { 
     res.json(notes);
   });
 
@@ -31,14 +36,14 @@ app.get('/', (req, res) => {
     fs.writeFileSync("./db/db.json", JSON.stringify(notes))
     res.json(notes)
   });
-
+  // request to delete by id and rewrite array
   app.delete("/api/notes/:id", function(req, res) {
     console.log("req params", req.params.id)
     notes = notes.filter(({ id }) => id !== req.params.id);
     fs.writeFileSync("./db/db.json", JSON.stringify(notes))
     res.json(notes)
   });
-
+// set up server to "listen" for requests
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
       });
